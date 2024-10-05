fcc890cb560ba49cb8ceef75a3c4b372de5aba00
shinyeeun <tlsdpdms789@naver.com>
2024-10-05 15:04:59 +0900

    백준 1008번문제 풀이

---
+++ step1/src/level1/Question2.java
@@ -0,0 +1,23 @@
+package level1;
+
+import java.util.*;
+
+// 1008번) 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+public class Question2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.nextLine().split(" ");
+        sc.close();
+
+        double a = Double.parseDouble(strArr[0]);
+        double b = Double.parseDouble(strArr[1]);
+
+        double result = a / b;
+
+        System.out.println(result);
+
+    }
+
+}
